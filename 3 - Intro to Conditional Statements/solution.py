2538950d7cf9b71c379ffc1549c8bc8de4e886c0
Arturo Milano <eulier1@gmail.com>
2019-05-21 21:12:08 -0400

    Update solution.py

--- 3 - Intro to Conditional Statements/solution.py
+++ 3 - Intro to Conditional Statements/solution.py
@@ -1,20 +1,11 @@
-#!/bin/python3
+N = int(input())
 
-import math
-import os
-import random
-import re
-import sys
-
-if __name__ == '__main__':
-    N = int(input())
-
-    if N % 2 != 0:
+if N % 2 != 0:
+    print("Weird")
+else:
+    if N <= 5:
+        print("Not Weird")
+    elif N <= 20:
         print("Weird")
     else:
-        if N <= 5:
-            print("Not Weird")
-        elif N <= 20:
-            print("Weird")
-        else:
-            print("Not Weird")
+        print("Not Weird")
