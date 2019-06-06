33af45794fca114a14852bf883ebffb3d3ec15a6
Arturo Milano <eulier1@gmail.com>
2019-06-06 08:27:33 -0400

    Handle EOF Exception

--- 8 - Dictionary & Maps/solution.py
+++ 8 - Dictionary & Maps/solution.py
@@ -8,9 +8,12 @@ for i in range(0, n):
     phone = int(data[1])
     phone_book[name] = phone
 
-for j in range(0, n):
-    name = str(input())
-    if name in phone_book:
-        print("{}={}".format(name,phone_book[name]))
-    else:
-        print("Not found")
+while True:
+    try:
+        name = str(input())
+        if name in phone_book:
+            print("{}={}".format(name,phone_book[name]))
+        else:
+            print("Not found")
+    except EOFError:
+        break
