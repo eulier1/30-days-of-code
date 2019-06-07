85894d22fe0002ec3d5a45f84fd8e7e86647282d
-Eu- <eulier1@gmail.com>
2019-06-07 09:38:29 -0400

    create solution.py

---
+++ I- 18 - Queue & Stack/solution.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+class Solution:
+    def __init__(self):
+        self.stack = list()
+        self.queue = deque(list())
+
+    def pushCharacter(self, ch):
+        self.stack.append(ch)
+
+    def enqueueCharacter(self, ch):
+        self.queue.append(ch)
+
+    def popCharacter(self):
+        return self.stack.pop()
+
+    def dequeueCharacter(self):
+        return self.queue.popleft()
